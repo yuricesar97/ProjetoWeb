659f3a62fc7112c334ddc0902aff3c96ebe778b3
Yuri Cesar <yuricesar97@gmail.com>
2020-05-28 23:27:40 -0300

    Projeto spring criado

---
+++ src/main/java/br/com/yuri/projetoweb/ProjetoWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.yuri.projetoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/yuri/projetoweb/ProjetoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.yuri.projetoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
