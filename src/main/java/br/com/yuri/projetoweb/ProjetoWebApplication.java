0a66554542320905d6343b91526c6b7b731218aa
Yuri Cesar <yuricesar97@gmail.com>
2020-05-31 20:24:31 -0300

    Criando operação de instanciação

--- src/main/java/br/com/yuri/projetoweb/ProjetoWebApplication.java
+++ src/main/java/br/com/yuri/projetoweb/ProjetoWebApplication.java
@@ -1,13 +1,31 @@
 package br.com.yuri.projetoweb;
 
+import br.com.yuri.projetoweb.domain.Categoria;
+import br.com.yuri.projetoweb.repositories.CategoriaRepository;
+import org.hibernate.mapping.Array;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetoWebApplication {
+import java.util.Arrays;
 
+@SpringBootApplication
+public class ProjetoWebApplication implements CommandLineRunner {
+	@Autowired
+	private CategoriaRepository cat;
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoWebApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Escritorio");
+
+		cat.saveAll(Arrays.asList(cat1,cat2));
+
+
+	}
 }
